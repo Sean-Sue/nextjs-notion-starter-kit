64bf656fd38f09bdc75bc7fddc0d3d784c01b9d5
Sean-Sue <162148045+Sean-Sue@users.noreply.github.com>
2024-03-04 23:08:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '3bd590ed65f2499f8908e118009a7235',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Sean'ShowReel',
+  domain: 'https://swift-seer-d6c.notion.site/Sean-Sue-Design-Portfolio-3bd590ed65f2499f8908e118009a7235',
+  author: 'Sean-Sue',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
